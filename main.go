a486875ce42556a3f650638f11c76503e21cbbd9
mobisUrbur <159678428+mobisUrbur@users.noreply.github.com>
2024-02-22 13:05:07 +0900

    Add files via upload

--- main.go
+++ main.go
@@ -1,6 +1,26 @@
 package main
 
+// import (
+// 	"context"
+// 	"fmt"
+// 	"net/http"
+// 	"sync"
+// 	"time"
+
+// 	"github.com/golang-jwt/jwt"
+// 	"github.com/google/uuid"
+// 	"github.com/jackc/pgx/v4/pgxpool"
+// 	"github.com/labstack/echo/v4"
+// 	"github.com/labstack/echo/v4/middleware"
+// )
+
+/*
+#cgo LDFLAGS: -L. -lurbr_go
+#include <stdlib.h>
+void inference(float const* from, float* to);
+*/
 import (
+	"C"
 	"context"
 	"fmt"
 	"net/http"
@@ -13,6 +33,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+import "unsafe"
 
 var (
 	alertMutex      sync.Mutex
@@ -53,7 +74,7 @@ func main() {
 
 	/////////// 보행자 관련 엔드포인트 등록 //////////////////////
 	// 보행자 미래 위치 100개 받고 충돌 위험 peduuid 배열 응답하는 핸들러
-	// e.POST("/pedHandler", PedHandler)
+	e.POST("/pedHandler", PedHandler)
 
 	// // 보행자 미래 위치 받는 엔드포인트 추가
 	e.POST("/updatePosition", updatePedestrainPosionHandler) // 기존 임시방편
@@ -83,43 +104,43 @@ type Inferenced struct {
 	From [100]float32 `json:"from"`
 }
 
-// // 보행자 미래 위치 100개 받고 충돌 위험 peduuid 배열 응답하는 핸들러
-// func PedHandler(c echo.Context) error {
-// 	var data Inferenced
+// // // 보행자 미래 위치 100개 받고 충돌 위험 peduuid 배열 응답하는 핸들러
+func PedHandler(c echo.Context) error {
+	var data Inferenced
 
-// 	if err := c.Bind(&data); err != nil {
-// 		return c.JSON(http.StatusBadRequest, Response{Message: "Invalid JSON format"})
-// 	}
+	if err := c.Bind(&data); err != nil {
+		return c.JSON(http.StatusBadRequest, Response{Message: "Invalid JSON format"})
+	}
 
-// 	from := data.From
+	from := data.From
 
-// 	// 추론 결과를 to 배열에 저장
-// 	C.inference(
-// 		(*C.float)(unsafe.Pointer(&from[0])),
-// 		(*C.float)(unsafe.Pointer(&to[0])))
+	// 추론 결과를 to 배열에 저장
+	C.inference(
+		(*C.float)(unsafe.Pointer(&from[0])),
+		(*C.float)(unsafe.Pointer(&to[0])))
 
-// 	// 추론 결과 출력
-// 	fmt.Println(to[0], to[1])
-// 	lat := to[0]
-// 	log := to[1]
+	// 추론 결과 출력
+	fmt.Println(to[0], to[1])
+	lat := to[0]
+	log := to[1]
 
-// 	fmt.Println("100개 데이터 인퍼런스드", from, lat, log)
+	fmt.Println("100개 데이터 인퍼런스드", from, lat, log)
 
-// 	// 받은 데이터를 사용하여 원하는 작업 수행
-// 	fmt.Printf("Received position update: userID %s, Latitude %f, Longitude %f\n", data.ID, lat, log)
+	// 받은 데이터를 사용하여 원하는 작업 수행
+	fmt.Printf("Received position update: userID %s, Latitude %f, Longitude %f\n", data.ID, lat, log)
 
-// 	// 데이터베이스에 위치 저장 함수 호출
-// 	if err := savePedPositionToDatabase(data.ID, lat, log); err != nil {
-// 		fmt.Println("데이터베이스에 위치 저장 중 오류 발생:", err)
-// 		// 오류 처리 필요에 따라 추가
-// 		return c.JSON(http.StatusInternalServerError, Response{Message: "데이터베이스에 위치 저장 중 오류 발생"})
-// 	}
+	// 데이터베이스에 위치 저장 함수 호출
+	if err := savePedPositionToDatabase(data.ID, lat, log); err != nil {
+		fmt.Println("데이터베이스에 위치 저장 중 오류 발생:", err)
+		// 오류 처리 필요에 따라 추가
+		return c.JSON(http.StatusInternalServerError, Response{Message: "데이터베이스에 위치 저장 중 오류 발생"})
+	}
 
-// 	// 충돌 위험 peduuid
-// 	PedestrianUUIDs, _ = getCollisionUUID()
+	// 충돌 위험 peduuid
+	PedestrianUUIDs, _ = getCollisionUUID()
 
-// 	return c.JSON(http.StatusOK, map[string]interface{}{"pedestrianUUIDs": PedestrianUUIDs})
-// }
+	return c.JSON(http.StatusOK, map[string]interface{}{"pedestrianUUIDs": PedestrianUUIDs})
+}
 
 // ////////////////////// 자동차 관련 //////////////////////////
 // 좌표를 저장하기 위한 구조체
